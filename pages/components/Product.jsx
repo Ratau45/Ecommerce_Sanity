a5a98ee32c27f0d0a0d183f264f373e8eed626d5
Ratau45 <71636622+Ratau45@users.noreply.github.com>
2023-02-13 22:47:20 +0200

    same thing

--- pages/components/Product.jsx
+++ pages/components/Product.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import { urlFor } from './Lib/client'
-
+import Image from 'next/image'
 
 
 const Product = ({product: {name, image, price, slug}}) => {
